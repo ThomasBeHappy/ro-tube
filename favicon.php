4b1f0a86943dd640b7ca578cd39c79e38bf10e39
CursedBlox <30377084+CursedBlox@users.noreply.github.com>
2017-07-22 16:47:12 -0400

    Add files via upload

--- favicon.php
+++ favicon.php
@@ -1,1 +1,2 @@
-<link rel="shortcut icon" href="http://www.faviconico.org/ico-converted/20170723/favicon-20170723034250968.ico" />
+<link rel="shortcut icon" type="icon" href="/favicon.ico"/>
+<link rel="shortcut icon" type="icon" href="http://www.faviconico.org/ico-converted/20170723/favicon-20170723034250968.ico"/>
